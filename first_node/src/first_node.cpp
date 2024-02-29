5fdf479c98f95f9d38e34439c6792b45291a9686
v_xchen_v <407081305@qq.com>
2024-02-29 12:21:52 +0800

    create your first node

---
+++ first_node/src/first_node.cpp
@@ -0,0 +1,10 @@
+// include necessary packages
+#include <ros/ros.h>
+
+int main(int argc, char *argv[])
+{
+    // ros init
+    ros::init(argc, argv, "first_node");
+    printf("Hello World!/n");
+    return 0;
+}
