f0a47ee0ee71469e54afea6a7ec6f7a03b5d0c0d
v_xchen_v <407081305@qq.com>
2024-02-29 13:36:34 +0800

    first publisher describer - impl a publisher

---
+++ first_publisher_subscriber/src/machao_node.cpp
@@ -0,0 +1,34 @@
+/*
+rosrun first_publisher_subscriber machao_node
+rostopic list
+rostopic hz kaihei_room
+rostopic echo kaihei_room
+*/
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+int main(int argc, char *argv[])
+{
+    // init with node with name
+    ros::init(argc, argv, "machao_ndoe");
+
+    // create a node handler who is a role of house keeper serving as the conduit for almost all forms of communication between nodes
+    ros::NodeHandle nodeHandle;
+
+    // create a publisher cares topic "kaihei_room"
+    ros::Publisher publisher = nodeHandle.advertise<std_msgs::String>("kaihei_room", 3);
+
+    // send message by 10 hz to topic
+    ros::Rate rate = ros::Rate(10);
+    while(ros::ok())
+    {
+        std_msgs::String msg;
+        msg.data = "Play a game?";
+        publisher.publish(msg);
+
+        rate.sleep();
+    }
+
+    return 0;
+}
